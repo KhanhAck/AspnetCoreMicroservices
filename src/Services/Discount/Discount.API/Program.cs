9668b29f087117089ca9a572a828babf122af2b7
Khanh <khanhack@outlook.com>
2022-09-25 18:48:31 +0700

    discount api

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,29 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+using Discount.API.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.Services.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
